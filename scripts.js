fbe6497f3ec0a23560550ab22f8b754bde5d86e8
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2023-12-06 11:53:52 -0600

    Added resizing functionality

--- scripts.js
+++ scripts.js
@@ -1,25 +1,38 @@
-
 const container = document.getElementById('container')
 
-
-for (let i=0; i<16; i++) {
-    let gridRow = document.createElement('div')
-    gridRow.className = 'gridRow'
-    
-    for (let i=0; i<16; i++) {
-        let gridSquare = document.createElement('div')
-        gridSquare.className = 'gridSquare'
-        gridRow.append(gridSquare)
+function drawGrid (sides=16) {
+    container.replaceChildren()
+    for (let i=0; i<sides; i++) {
+        let gridRow = document.createElement('div')
+        gridRow.className = 'gridRow'
+        
+        for (let i=0; i<sides; i++) {
+            let gridSquare = document.createElement('div')
+            gridSquare.className = 'gridSquare'
+            gridRow.append(gridSquare)
+        }
+        container.appendChild(gridRow)
     }
-    container.appendChild(gridRow)
+    resizeGrid(sides)
+    addPen()
 }
 
-//need a function to set border for right and bottom, nth child
+function addPen() {
+    const allGridSquares = document.querySelectorAll('.gridSquare')
 
-const allGridSquares = document.querySelectorAll('.gridSquare')
-
-allGridSquares.forEach((item) => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'black'
+    allGridSquares.forEach((item) => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'black'
+        })
     })
-})
+}
+
+function resizeGrid (sides=16) {
+    let rows = document.querySelectorAll('.gridRow')
+    rows.forEach((row) => row.style.height = (100/sides)+'%')
+
+    let squares = document.querySelectorAll('.gridSquare')
+    squares.forEach((square) => square.style.width = (100/sides)+'%')
+}
+
+drawGrid()
